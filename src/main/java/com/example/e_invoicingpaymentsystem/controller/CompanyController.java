17837eb2e0ef1fb8e611e90798d9680489ba44ed
Lilit <amiryanlilart@gmail.com>
2022-06-09 12:16:13 +0400

    CompanyController

--- src/main/java/com/example/e_invoicingpaymentsystem/controller/CompanyController.java
+++ src/main/java/com/example/e_invoicingpaymentsystem/controller/CompanyController.java
@@ -1,9 +1,36 @@
 package com.example.e_invoicingpaymentsystem.controller;
 
+import com.example.e_invoicingpaymentsystem.dto.CompanyDto;
+import com.example.e_invoicingpaymentsystem.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@RequestMapping("/api/company")
 public class CompanyController {
 
+    CompanyService companyService;
+
+    @Autowired
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> createCompany(@RequestBody CompanyDto companyDto) {
+
+        if (companyDto == null) {
+            return new ResponseEntity<>(
+                    "Please enter all fields", HttpStatus.BAD_REQUEST);
+        }
+        return companyService.createCompany(companyDto);
+    }
 
 }
