d480a98d1965a7ce990b8484f5fe4bd7aeed46d5
Lilit <amiryanlilart@gmail.com>
2022-06-09 12:21:28 +0400

    CompanyValidator

---
+++ src/main/java/com/example/e_invoicingpaymentsystem/validator/CompanyValidator.java
@@ -0,0 +1,19 @@
+package com.example.e_invoicingpaymentsystem.validator;
+
+import com.example.e_invoicingpaymentsystem.dto.CompanyDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CompanyValidator {
+    public boolean isValidCompany(CompanyDto companyDto ) {
+
+        return (!(companyDto.getTin() == null ||
+                companyDto.getCompanyName() == null ||
+                companyDto.getCompAccountNumber() == null ||
+                companyDto.getEmail() == null ||
+                companyDto.getPhoneNumber() == null ||
+                companyDto.getPassword()==null
+        )
+        );
+    }
+}
