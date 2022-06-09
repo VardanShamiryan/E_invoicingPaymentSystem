6b2a0e52d3b474273552ce2c250f57245f615704
Lilit <amiryanlilart@gmail.com>
2022-06-09 12:30:28 +0400

    Company create

--- src/main/java/com/example/e_invoicingpaymentsystem/dto/CompanyDto.java
+++ src/main/java/com/example/e_invoicingpaymentsystem/dto/CompanyDto.java
@@ -1,9 +1,12 @@
 package com.example.e_invoicingpaymentsystem.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 
 @Getter
 @Setter
@@ -16,5 +19,17 @@ public class CompanyDto {
     private String compAccountNumber;
     private String email;
     private String phoneNumber;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
 
+    @JsonProperty
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
 }
+
