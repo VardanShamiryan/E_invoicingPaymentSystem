862d5ba90ce92820dce4d55999155f5ec70e1eac
Lilit <amiryanlilart@gmail.com>
2022-06-09 12:23:21 +0400

    aes

---
+++ src/main/java/com/example/e_invoicingpaymentsystem/mapper/AES.java
@@ -0,0 +1,50 @@
+package com.example.e_invoicingpaymentsystem.mapper;
+
+package com.example.e_invoicingpaymentsystem.mapper;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import java.util.Base64;
+
+class AES {
+
+    private SecretKey key;
+    private final int KEY_SIZE = 128;
+    private final int T_LEN = 128;
+
+    public String decrypt(String encryptedMessage) throws Exception {
+        byte[] messageInBytes = decode(encryptedMessage);
+        Cipher decryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        GCMParameterSpec spec = new GCMParameterSpec(T_LEN, encryptionCipher.getIV());
+        decryptionCipher.init(Cipher.DECRYPT_MODE, key, spec);
+        byte[] decryptedBytes = decryptionCipher.doFinal(messageInBytes);
+        return new String(decryptedBytes);
+    }
+
+    private Cipher encryptionCipher;
+
+    public String encrypt(String message) throws Exception {
+        byte[] messageInBytes = message.getBytes();
+        encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        encryptionCipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedBytes = encryptionCipher.doFinal(messageInBytes);
+        return encode(encryptedBytes);
+    }
+
+    private String encode(byte[] data) {
+        return Base64.getEncoder().encodeToString(data);
+    }
+
+    private byte[] decode(String data) {
+        return Base64.getDecoder().decode(data);
+    }
+
+    public void init() throws Exception {
+        KeyGenerator generator = KeyGenerator.getInstance("AES");
+        generator.init(KEY_SIZE);
+        key = generator.generateKey();
+    }
+
+}
