cdfb1336052c268c0f8d260fefd0381f836d003d
Yura <yura.mkrtchyan01@gmail.com>
2022-06-10 17:41:08 +0400

    payment order finished again

--- src/main/java/com/example/e_invoicingpaymentsystem/controller/PaymentOrderController.java
+++ src/main/java/com/example/e_invoicingpaymentsystem/controller/PaymentOrderController.java
@@ -1,14 +1,12 @@
 package com.example.e_invoicingpaymentsystem.controller;
 
 import com.example.e_invoicingpaymentsystem.service.PaymentOrderService;
-import org.apache.http.HttpResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.bind.JAXBException;
 @RestController
 @RequestMapping("/api/payment")
 
@@ -21,12 +19,9 @@ public class PaymentOrderController {
     }
 
     @PutMapping("/transfer")
-    public ResponseEntity<?> transferFromAccountToAccount(@RequestParam Double amount,
-                                                       @RequestParam String fromAccountNumber,
-                                                       @RequestParam String toAccountNumber) {
-        return paymentOrderService.transferFromAccountToAccount(amount,
-                fromAccountNumber,
-                toAccountNumber);
+    public ResponseEntity<?> paymentOrder(@RequestParam Double amount,
+                                          @RequestParam String invoiceNumber) {
+        return paymentOrderService.paymentOrder(amount, invoiceNumber);
     }
 
 }
--- src/main/java/com/example/e_invoicingpaymentsystem/service/PaymentOrderService.java
+++ src/main/java/com/example/e_invoicingpaymentsystem/service/PaymentOrderService.java
@@ -1,37 +1,64 @@
 package com.example.e_invoicingpaymentsystem.service;
 
 import com.example.e_invoicingpaymentsystem.dto.Order;
+import com.example.e_invoicingpaymentsystem.model.Debt;
+import com.example.e_invoicingpaymentsystem.model.Invoice;
+import com.example.e_invoicingpaymentsystem.repository.CompanyRepository;
+import com.example.e_invoicingpaymentsystem.repository.DebtRepository;
+import com.example.e_invoicingpaymentsystem.repository.InvoiceRepository;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Objects;
+
 @Service
 public class PaymentOrderService {
 
-    public ResponseEntity<?> transferFromAccountToAccount(
+    @Autowired
+    InvoiceRepository invoiceRepository;
+    @Autowired
+    DebtRepository debtRepository;
+//    @Autowired
+//    CompanyRepository companyRepository;
+
+    public ResponseEntity<?> paymentOrder(
             Double amount,
-            String fromAccountNumber,
-            String toAccountNumber) {
+            String invoiceNumber) {
 
+        Invoice invoice = invoiceRepository.getInvoiceByInvoiceNumber(invoiceNumber);
+        Debt debt = debtRepository.findDebtByCompanyAndSupplier(invoice.getCompany(), invoice.getSupplier());
 
-        Order order = new Order(amount, //manually
-                fromAccountNumber, //
-                toAccountNumber);
+                String fromAccountNumber = invoice.getCompany().getCompAccountNumber();
+        String toAccountNumber = invoice.getSuppAccountNumber();
+
+        Order order = new Order(amount, fromAccountNumber, toAccountNumber);
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String json = gson.toJson(order);
 
-        String url = "http://192.168.10.83:8080/api/account/transferAccountAccount";
-        //String url = "http://localhost:8080/api/account/transferAccountAccount";
+//        String url = "http://192.168.10.83:8080/api/account/transferAccountAccount";
+        String url = "http://localhost:8080/api/account/transferAccountAccount";
 
         RestTemplate template = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        HttpEntity<String> entity = new HttpEntity<>(json,headers);
+        HttpEntity<String> entity = new HttpEntity<>(json, headers);
 
-        return template.exchange(url, HttpMethod.PUT, entity, String.class);
+        ResponseEntity<?> response = template.exchange(url, HttpMethod.PUT, entity, String.class);
+
+        if (response.getStatusCode().value() == 200) {
+            invoice.setInvoiceDebt(invoice.getInvoiceDebt() - amount);
+            invoiceRepository.save(invoice);
+            debt.setTotal_debt(debt.getTotal_debt() - amount);
+            debtRepository.save(debt);
+
+        }
+
+        return response;
     }
 }
